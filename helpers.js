e5de371e03152e4bfb3368af194c4a6454cc4c28
surralles <101697565+surralles@users.noreply.github.com>
2022-08-30 11:49:47 +0200

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -3,5 +3,5 @@ export const capitalize =(word)=>{
 }
 
 export const calculateCalories = (carbs=0, protein=0, fat=0) => {
-  return  carbs*4 + protein*4 + fat*9
+  return  (carbs*4 + protein*4 + fat*9);
 }
