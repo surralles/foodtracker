3bf158af8970f6c9d23984b84b92c4c3811a687a
surralles <101697565+surralles@users.noreply.github.com>
2022-08-30 10:46:14 +0200

    Update index.js
    Add HTML

--- index.js
+++ index.js
@@ -2,29 +2,51 @@
 import FetchWrapper from "./fetch-wrapper.js"
 const API = new FetchWrapper ("https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents/projecteFood");
 
-const form = document.querySelector ("#create-form");
-const name = document.querySelector ("#create-name");
-const carbs = document.querySelector ("#create-carbs");
-const protein = document.querySelector ("#create-protein");
-const fat = document.querySelector ("#create-fat");
+const list = document.querySelector ("#food-list");
+const form = document.querySelector("#create-form");
+const name = document.querySelector("#create-name");
+const carbs = document.querySelector("#create-carbs");
+const protein = document.querySelector("#create-protein");
+const fat = document.querySelector("#create-fat");
 
-form.addEventListener ("submit", (event) => {
-    event.preventDefault();
-    API.post ("/",{
-         fields: {
-        name : {stringValue: name.value},
-        carbs : {integerValue: carbs.value},
-        protein :{integerValue: protein.value},
-        fat :{integerValue:fat.value},
-    },}).then (data =>{
-        console.log(data);
-         if (data.error) {
-    
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  API.post("/", {
+    fields: {
+      name: { stringValue: name.value },
+      carbs: { integerValue: carbs.value },
+      protein: { integerValue: protein.value },
+      fat: { integerValue: fat.value },
+    },
+  }).then((data) => {
+    console.log(data);
+    if (data.error) {
+        // there was an error
         return;
     }
-        name.value="";
-        carbs.value="";
-        protein.value="";
-        fat.value="";
-    })
+
+    // Add new food to the list
+     
+   list.insertAdjacentHTML("beforeend",
+    `<li class="card">
+  <div>
+    <h3 class="name">${name.value}</h3>
+    <div class="calories">0 calories</div>
+    <ul class="macros">
+      <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
+      <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
+      <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
+    </ul>
+  </div>
+</li>`)
+
+        // Clear values
+      
+ name.value = "";
+    carbs.value = "";
+    protein.value = "";
+    fat.value = "";
+
+  });
 });
