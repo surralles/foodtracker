486c869fbd0396669099a964c28b1054c60ea97e
surralles <101697565+surralles@users.noreply.github.com>
2022-08-31 15:26:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import FetchWrapper from "./fetch-wrapper.js";
-import {capitalize, calculateCalories} from "./helpers.js"
+import { capitalize, calculateCalories } from "./helpers.js";
+import snackbar from "snackbar";
+import AppData from "./app-data.js"
 
 const API = new FetchWrapper(
   "https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents/projecteFood"
 );
+
+const appData = new AppData;
 
 const list = document.querySelector("#food-list");
 const form = document.querySelector("#create-form");
@@ -11,6 +15,27 @@ const name = document.querySelector("#create-name");
 const carbs = document.querySelector("#create-carbs");
 const protein = document.querySelector("#create-protein");
 const fat = document.querySelector("#create-fat");
+
+const displayEntry = (name, carbs, protein, fat) => {
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<li class="card">
+        <div>
+          <h3 class="name">${capitalize(name)}</h3>
+          <div class="calories">${calculateCalories(
+            carbs,
+            protein,
+            fat
+          )} calories</div>
+          <ul class="macros">
+            <li class="carbs"><div>Carbs</div><div class="value">${carbs}g</div></li>
+            <li class="protein"><div>Protein</div><div class="value">${protein}g</div></li>
+            <li class="fat"><div>Fat</div><div class="value">${fat}g</div></li>
+          </ul>
+        </div>
+      </li>`
+  );
+};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -25,32 +50,37 @@ form.addEventListener("submit", (event) => {
   }).then((data) => {
     console.log(data);
     if (data.error) {
-        // there was an error
-        return;
+      // there was an error
+      snackbar.show("Some data is missing.");
+      return;
     }
 
- // Add new food to the list
-  // import function from helpers
+    snackbar.show("Food added successfully.");
 
-    list.insertAdjacentHTML(
-        "beforeend",
-        `<li class="card">
-          <div>
-            <h3 class="name">${capitalize(name.value)}</h3>
-            <div class="calories">${calculateCalories(carbs.value, protein.value, fat.value)} calories</div>
-            <ul class="macros">
-              <li class="carbs"><div>Carbs</div><div class="value">${capitalize(carbs.value)}g</div></li>
-              <li class="protein"><div>Protein</div><div class="value">${capitalize(protein.value)}g</div></li>
-              <li class="fat"><div>Fat</div><div class="value">${capitalize(fat.value)}g</div></li>
-            </ul>
-          </div>
-        </li>`
-    );
+    
 
-  // Clear values
+    displayEntry(name.value, carbs.value, protein.value, fat.value);
+
     name.value = "";
     carbs.value = "";
     protein.value = "";
     fat.value = "";
   });
 });
+
+const init = () => {
+    API.get("/?pageSize=100").then((data) => {
+      data.documents?.forEach((doc) => {
+        const fields = doc.fields;
+
+        displayEntry(
+          fields.name.stringValue,
+          fields.carbs.integerValue,
+          fields.protein.integerValue,
+          fields.fat.integerValue
+        );
+      });
+    });
+}
+
+init();
